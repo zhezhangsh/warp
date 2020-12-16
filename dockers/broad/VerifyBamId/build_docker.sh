989860bb40bfeeb195a968d9f1868b6941509786
gbggrant <ggrant@broadinstitute.org>
2020-12-16 13:51:27 -0500

    Update VerifyBamId docker for Yossi's offline needs. Not currently adding this to production WDLs

--- dockers/broad/VerifyBamId/build_docker.sh
+++ dockers/broad/VerifyBamId/build_docker.sh
@@ -2,9 +2,9 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-VERIFY_BAM_ID_COMMIT="c1cba76e979904eb69c31520a0d7f5be63c72253"
+VERIFY_BAM_ID_COMMIT="f6cb51761861e57c43879aa262df5cf8e670cf7c"
 TIMESTAMP=$(date +"%s")
 DOCKER_TAG="$VERIFY_BAM_ID_COMMIT-$TIMESTAMP"
 
 docker build -t us.gcr.io/broad-gotc-prod/verify-bam-id:${DOCKER_TAG} --build-arg GIT_HASH="$VERIFY_BAM_ID_COMMIT" "$DIR"
-gcloud docker -- push us.gcr.io/broad-gotc-prod/verify-bam-id:${DOCKER_TAG}
+docker push us.gcr.io/broad-gotc-prod/verify-bam-id:${DOCKER_TAG}
